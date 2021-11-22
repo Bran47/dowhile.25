8abacd05a704ab842f6c3310ef9e7668ad772f3e
Bran47 <93209532+Bran47@users.noreply.github.com>
2021-11-22 22:44:38 +0300

    Add files via upload

---
+++ do.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+ 	{
+	 
+	int number = 20;
+	int sum = 0;
+	do{ 
+	cout<<"20/n";
+	sum += number;
+	cout<<"sum is: "<<sum;
+	number++;
+	}
+	while(number<=25);
+
+return 0;
+}
+
+	
+
+
